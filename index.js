e81cce2e5a49a11ec45f533e9013b4804dab8fb6
sosuke-k <snoopies.drum@gmail.com>
2020-02-14 23:22:40 +0900

    let index.html load bundle.js using defer

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import './App.scss';
 import App from './App.svelte';
 
-window.app = new App({
-  target: document.getElementsByTagName('app')[0]
+// window.app = new App({
+//   target: document.getElementsByTagName('app')[0]
+// });
+
+const app = new App({
+  target: document.body
 });
+
+export default app;
